aff10d7024ba892e2d9df72a1567f0812695a3ca
rm86330 <rm86330@estudante.fieb.edu.br>
2023-06-13 09:18:47 -0300

    13/06

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -2,11 +2,44 @@ package com.itb.inf2dm.comercio.model;
 
 public class Produto {
 private long id;
-private String descriçao;
+private String descricao;
 private String nome;
 private String codigoBarras;
 private Double precos;
-	
-	
 
+   //public: modificador de acesso livre para todas as classes
+   //private: modificador de acesso restrito apenas para membros da
+   //propria classe. entende-se por "membros " (atributos e / ou metodos)
+
+   //Metodos setter's e getter's : Adicionar e Recuperar dados do atributo"Respectivamente"
+public void setId(long id) {
+	this.id = id;
+}
+public long getId() {
+	return id;
+}
+public String getDescricao() {
+	return descricao;
+}
+public void setDescricao(String descricao) {
+	this.descricao = descricao;
+}
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getCodigoBarras() {
+	return codigoBarras;
+}
+public void setCodigoBarras(String codigoBarras) {
+	this.codigoBarras = codigoBarras;
+}
+public Double getPrecos() {
+	return precos;
+}
+public void setPrecos(Double precos) {
+	this.precos = precos;
+}
 }
--- src/main/java/com/itbinf2dm/comercio/controller/lojaController.java
+++ src/main/java/com/itbinf2dm/comercio/controller/lojaController.java
@@ -6,13 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class lojaController {
+	Lista<Produto> listadeprodutos = new Arraylist<Produto>();
 	
 @GetMapping("/listar")
 public String listarProdutos() {
+	Produto p1 = new Produto();
+	p1.id = 20l;
+	p1.setNome("Máquina de Lavar Brastemp 12 litros");
+	p1.setCodigoBarras("JGIEI453JD2KMEU5311");
+	p1.setprecos(123445.32);
+	
+	listadeProdutos.add(p1);
 	return "produtos";
 	
 }
-
+@GetMapping("/novo-prod")
+public String novoProduto() {
+	return "novo-prod";
+}
 	
 
 }
