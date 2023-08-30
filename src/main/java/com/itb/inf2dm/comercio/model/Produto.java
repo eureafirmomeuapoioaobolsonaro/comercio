c9211199cc97eded75741fc865e1a68be51b2af5
rm86330 <rm86330@estudante.fieb.edu.br>
2023-08-30 07:23:17 -0300

    escreve oq

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -4,7 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+  
 @Entity
 @Table(name="produtos")
 public class Produto {
--- src/main/java/com/itbinf2dm/comercio/controller/lojaController.java
+++ src/main/java/com/itbinf2dm/comercio/controller/lojaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itb.inf2dm.comercio.model.Produto;
-
+ 
 @Controller
 @RequestMapping("/comercio/produtos")   
 public class lojaController {
